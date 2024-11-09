0c4faa85e76cee7c07be01a8d830512dbcef4a24
Andrey Podovinov <podovinov2003andrey@gmail.com>
2024-11-09 17:30:48 +0600

    refactor

--- backend/src/utils/ner.py
+++ backend/src/utils/ner.py
@@ -1,9 +1,9 @@
-import itertools
 import re
 import typing as t
+from itertools import chain
 
 
-_patterns = [
+_DEFAULT_PATTERNS = [
     r'\b[A-Za-zА-Яа-я]\d{9}\b(?:\([^\)]*\))?',
     r'\b[A-Za-zА-Яа-я]{3}\d{11}\b(?:\([^\)]*\))?',
 ]
@@ -36,52 +36,70 @@ def id_preprocessing(id: str) -> str:
     return russian_to_english(id.upper())
 
 
-def get_all_serial_numbers(header: str, body: str, patterns: t.List[str] = None, preprocessing: t.Callable[[str], str] = lambda s: s, default_value: str = 'Уточнить') -> t.List[str]:
+def get_all_serial_numbers(
+    header: str,
+    body: str,
+    patterns: t.Iterable[str] | None = None,
+    preprocessing: t.Callable[[str], str] = lambda s: s,
+    default_value: str = 'Уточнить',
+) -> list[str]:
     """Возвращает все найденные Серийные номера в тексте заголовка письма и его тела
 
     Args:
         header (str): Заголовок письма
         body (str): Тело письма
-        patterns (t.List[str]): Признаки кластеров Серийных номеров
-        preprocessing (_type_, optional): Функция по предобработке результирующих Серийных номеров. Defaults to lambdas:s.
+        patterns (t.Iterable[str], optional): Признаки кластеров Серийных номеров. Defaults to _DEFAULT_PATTERNS
+        preprocessing (t.Callable[[str], str], optional): Функция по предобработке результирующих Серийных номеров. Defaults to (lambda s: s)
+        default_value (str, optional): Значение, которое буде возвращено, если Серийные номера не будут найдены. Defaults to Уточнить
 
     Returns:
-        t.List[str]: Список всех найденных Серийных номеров в письме
+        list[str]: Список всех найденных Серийных номеров в письме
     """
     if patterns is None:
-        patterns = _patterns
+        patterns = _DEFAULT_PATTERNS
 
     res = list(
         map(
-            lambda s: preprocessing(s),
-            itertools.chain(*[
+            preprocessing,
+            chain(*(
                 set(re.findall(pattern, header) + re.findall(pattern, body))
                 for pattern in patterns
-            ])
+            ))
         )
     )
+
     if res:
         return res
+
     return [default_value]
 
 
-def get_one_serial_number(header: str, body: str, patterns: t.List[str] = None, preprocessing: t.Callable[[str], str] = lambda s: s, default_value: str = 'Уточнить') -> str:
+def get_one_serial_number(
+    header: str,
+    body: str,
+    patterns: t.Iterable[str] | None = None,
+    preprocessing: t.Callable[[str], str] = lambda s: s,
+    default_value: str = 'Уточнить',
+) -> str:
     """Возвращает первое вхождение Серийного номера в тексте заголовка письма и его тела
 
     Args:
         header (str): Заголовок письма
         body (str): Тело письма
-        patterns (t.List[str]): Признаки кластеров Серийных номеров
-        preprocessing (_type_, optional): Функция по предобработке результирующих Серийных номеров. Defaults to lambdas:s.
+        patterns (t.Iterable[str], optional): Признаки кластеров Серийных номеров. Defaults to _DEFAULT_PATTERNS
+        preprocessing (t.Callable[[str], str], optional): Функция по предобработке результирующих Серийных номеров. Defaults to (lambda s: s)
+        default_value (str, optional): Значение, которое буде возвращено, если Серийные номера не будут найдены. Defaults to Уточнить
 
     Returns:
-        t.List[str]: Первое вхождение Серийного номера в письме
+        str: Первое вхождение Серийного номера в письме
     """
     if patterns is None:
-        patterns = _patterns
+        patterns = _DEFAULT_PATTERNS
 
     agg_pattern = '|'.join(patterns)
     res = re.findall(agg_pattern, ' '.join((header, body)))
+
     if res:
         return preprocessing(res[0])
+
     return default_value
