712116598c497829b324116a2e685a43827218bc
Andrey Podovinov <podovinov2003andrey@gmail.com>
2024-11-10 04:50:16 +0600

    fix utils

--- backend/src/utils/check_equipment_name.py
+++ backend/src/utils/check_equipment_name.py
@@ -3,10 +3,10 @@ import os
 from langchain_ollama.llms import OllamaLLM
 from langchain.prompts import ChatPromptTemplate
 
-from .templates import CHECK_COMPLETE_INFO_TEMPLATE
+from .templates import CHECK_EQUIPMENT_NAME_TEMPLATE
 
 
-_prompt = ChatPromptTemplate.from_template(CHECK_COMPLETE_INFO_TEMPLATE)
+_prompt = ChatPromptTemplate.from_template(CHECK_EQUIPMENT_NAME_TEMPLATE)
 
 _llm = OllamaLLM(
     model='gemma2:9b',
@@ -20,13 +20,11 @@ _llm = OllamaLLM(
 _chain = _prompt | _llm
 
 
-async def check_complete_information(title: str, body: str, type_of_equipment : str, fail_point: str) -> str:
-    """ Проверяет с помощью модель gemma2 полна ли информация в сообщении
+async def check_equipment_name(title: str, body: str) -> str:
+    """ Проверяет с помощью модель gemma2 есть ли в тексте названия оборудования
     Args:
         title (str): Заголовок письма
         body (str): Текст письма
-        type_of_equipment (str): Тип оборудования (определен классификаторами)
-        fail_point (str): Точка отказа (определена классификаторами)
 
     Returns:
         str: Результат работы модели в виде json
@@ -34,12 +32,10 @@ async def check_complete_information(title: str, body: str, type_of_equipment : 
             'flag': ...,
             'answer': ...
         }, где flag (boolean) - true значит, что название присутствует, flag - false, что отсутствует
-        answer (str) - ответный запрос модели (непустой, если flag - false).
+        answer (str) - ответный запрос модели, (непустой, если flag - false).
     """
 
     return await _chain.ainvoke({
         'title': title,
-        'body': body,
-        'type_of_equipment': type_of_equipment,
-        'fail_point': fail_point,
+        'body': body
     })
---
+++ backend/src/utils/nlu.py
@@ -0,0 +1,141 @@
+import joblib
+
+import pandas as pd
+import torch
+import onnxruntime as ort
+from transformers import AutoTokenizer, AutoModel
+from sklearn.preprocessing import LabelEncoder
+
+
+tokenizer = AutoTokenizer.from_pretrained('intfloat/multilingual-e5-large-instruct')
+model_embedding = AutoModel.from_pretrained('intfloat/multilingual-e5-large-instruct')
+model_classification_equipment_type = joblib.load('./models/cb_equipment_model.pkl')
+
+label_encoder: LabelEncoder = joblib.load('./models/label_encoder.pkl')
+model_classification_failure_point = ort.InferenceSession('./models/best_model.onnx', providers=['CPUExecutionProvider'])
+
+
+def average_pool(last_hidden_states: torch.Tensor, attention_mask: torch.Tensor) -> torch.Tensor:
+    ''' 
+    Функция average_pool выполняет усреднение скрытых состояний последнего слоя модели, 
+    учитывая маску внимания. 
+
+    Параметры:
+    - last_hidden_states: Тензор, содержащий скрытые состояния последнего слоя модели. 
+      Размерность тензора - (batch_size, sequence_length, hidden_size).
+    - attention_mask: Тензор, указывающий, какие токены следует учитывать при усреднении. 
+      Размерность тензора - (batch_size, sequence_length).
+
+    Возвращает:
+    - Тензор, представляющий усредненные эмбеддинги для каждого примера в батче. 
+      Размерность тензора - (batch_size, hidden_size).
+    
+    Процесс:
+    1. Скрытые состояния, соответствующие токенам, которые не должны учитываться (например, паддинг), 
+       заменяются на нули с помощью метода masked_fill.
+    2. Затем выполняется суммирование скрытых состояний по временной оси (dim=1).
+    3. Наконец, результат делится на количество токенов, которые были учтены (сумма маски внимания), 
+       чтобы получить усредненные эмбеддинги.
+    '''
+    last_hidden = last_hidden_states.masked_fill(~attention_mask[..., None].bool(), 0.0)
+    return last_hidden.sum(dim=1) / attention_mask.sum(dim=1)[..., None]
+
+
+def create_embeddings(topic: str, description: str, model, tokenizer) -> torch.Tensor:
+    text_input = f"{topic} {description}"
+
+    batch_dict = tokenizer([text_input], max_length=512, padding=True, truncation=True, return_tensors='pt')
+
+    with torch.no_grad():
+        outputs = model(**batch_dict)
+
+    embeddings = average_pool(outputs.last_hidden_state, batch_dict['attention_mask'])
+    embeddings = torch.nn.functional.normalize(embeddings, p=2, dim=1)
+
+    return embeddings
+
+
+def generate_prediction_equipment_type(topic: str, description: str, model, tokenizer, cb_model) -> str:
+    ''' 
+    Функция generate_prediction генерирует предсказание темы обращения для заданного текста, 
+    состоящего из темы и описания, используя предобученную модель и токенизатор.
+
+    Параметры:
+    - topic: Строка, представляющая тему текста, для которого необходимо сделать предсказание.
+    - description: Строка, представляющая описание текста, для которого необходимо сделать предсказание.
+    - model: Предобученная модель, используемая для получения эмбеддингов текста.
+    - tokenizer: Токенизатор, используемый для преобразования текста в формат, 
+      подходящий для модели.
+    - cb_model: Модель, используемая для генерации предсказаний на основе эмбеддингов.
+
+    Возвращает:
+    - str: Предсказанный тип обращения для заданного текста.
+
+    Процесс:
+    1. Формируется текстовый ввод, объединяя тему и описание.
+    2. Текст токенизируется и преобразуется в тензоры, подходящие для модели, с учетом 
+       максимальной длины, паддинга и обрезки.
+    3. С помощью модели получаются скрытые состояния, при этом отключается градиент 
+       для экономии памяти и ускорения вычислений.
+    4. Вызывается функция average_pool для получения усредненных эмбеддингов на основе 
+       скрытых состояний и маски внимания.
+    5. Эмбеддинги нормализуются для улучшения качества предсказаний.
+    6. На основе нормализованных эмбеддингов генерируется предсказание с помощью cb_model.
+    7. Возвращается предсказанный класс.
+    '''
+
+    embeddings = create_embeddings(topic, description, model, tokenizer)
+
+    prediction = cb_model.predict(embeddings.numpy())
+
+    return str(prediction[0][0])
+
+
+def generate_predictions_from_dataframe(df: pd.DataFrame, model, tokenizer, cb_model) -> pd.Series:
+    ''' 
+    Функция generate_predictions_from_dataframe генерирует предсказания для каждого 
+    текста в DataFrame, используя предобученную модель и токенизатор.
+
+    Параметры:
+    - df: DataFrame, содержащий данные с текстами, для которых необходимо сделать предсказания. 
+      Ожидается, что в DataFrame есть столбцы 'Тема' и 'Описание'.
+    - model: Предобученная модель, используемая для получения эмбеддингов текста.
+    - tokenizer: Токенизатор, используемый для преобразования текстов в формат, 
+      подходящий для модели.
+    - cb_model: Модель, используемая для генерации предсказаний на основе эмбеддингов.
+
+    Возвращает:
+    - pd.Series: Серия с предсказанными классами для каждого текста в DataFrame.
+
+    Процесс:
+    1. Инициализируется пустой список для хранения предсказаний.
+    2. Для каждой строки в DataFrame:
+       - Формируется текстовый ввод, объединяя 'Тема' и 'Описание'.
+       - Текст токенизируется и преобразуется в тензоры, подходящие для модели.
+       - С помощью модели получаются скрытые состояния.
+       - Вызывается функция average_pool для получения усредненных эмбеддингов.
+       - Эмбеддинги нормализуются.
+       - На основе нормализованных эмбеддингов генерируется предсказание с помощью cb_model.
+       - Предсказание добавляется в список.
+    3. Возвращается серия с предсказанными классами.
+    '''
+    predictions = []
+
+    for index, row in df.iterrows():
+        embeddings = create_embeddings(row['Тема'], row['Описание'], model, tokenizer)
+
+        prediction = cb_model.predict(embeddings.numpy())
+        predictions.append(prediction[0][0])
+
+    return pd.Series(predictions)
+
+
+def generate_prediction_failure_point(topic: str, description: str, model_embedding, tokenizer, model_onnx: ort.InferenceSession, label_encoder) -> str:
+    embeddings = create_embeddings(topic, description, model_embedding, tokenizer)
+
+    outputs = model_onnx.run(None, {'input': embeddings.reshape(1, 1, -1).numpy()})
+    pred = outputs[0].argmax(axis=1)
+
+    cls = label_encoder.inverse_transform(pred)
+
+    return cls.item()
